21622ec6a1e520e8501d448e0da343ac54edbbbc
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-09-04 15:51:28 +0300

    Home page is ready

---
+++ js/main.js
@@ -0,0 +1,69 @@
+window.onscroll = function() {fixHeader()}
+
+let header = document.querySelector('.header')
+let sticky = header.offsetTop
+
+function fixHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("header--zindex")
+  } else {
+    header.classList.remove("header--zindex")
+  }
+}
+
+function openMenu(event) {
+    event.preventDefault()
+    const btnMenu = document.querySelector('.menu__btn');
+    const listMenu = document.querySelector('.menu__list');
+    const btnElement = document.querySelector('.menu__btn__element');
+    btnMenu.classList.toggle('menu__btn--close');
+    listMenu.classList.toggle('menu__list--active');
+    btnElement.classList.toggle('menu__btn__element--close');
+}
+
+$(document).ready(function() {
+  $('.top__slider').slick({
+		mobileFirst: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		rows: 1,
+		arrows: true,
+		dots: false,
+		autoplay: true,
+		infinite: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          dots: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 280,
+        settings: {
+          dots: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+	});
+})
